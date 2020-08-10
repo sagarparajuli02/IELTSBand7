0d13c88d53dd6a729d377ca2efaafbf900e42a0c
Sagar Parajuli <official.sagar@yahoo.com>
2020-08-10 21:33:46 +1000

    Writing and Listening Task Under Progress

---
+++ app/src/main/java/com/ielts/reading/listening/writing/speaking/WritingAdapter.java
@@ -0,0 +1,41 @@
+package com.ielts.reading.listening.writing.speaking;
+
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class WritingAdapter extends FragmentPagerAdapter {
+
+    private Context myContext;
+    int totalTabs;
+
+    public WritingAdapter(Context context, FragmentManager fm, int totalTabs) {
+        super(fm);
+        myContext = context;
+        this.totalTabs = totalTabs;
+    }
+
+    // this is for fragment tabs
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                TaskOneFragment homeFragment = new TaskOneFragment();
+                return homeFragment;
+            case 1:
+                TaskTwoFragment sportFragment = new TaskTwoFragment();
+                return sportFragment;
+
+            default:
+                return null;
+        }
+    }
+    // this counts total number of tabs
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
